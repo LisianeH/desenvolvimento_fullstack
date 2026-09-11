3d2451495c81b46f44fb1eca13354203c666b5ab
LISIANE HOFFMEISTER <04339277045@senacrs.edu.br>
2026-09-10 21:47:54 -0300

    aula 06 continue

--- aula05/FRONTEND/js/script.js
+++ aula05/FRONTEND/js/script.js
@@ -2,6 +2,11 @@ const listCategories = document.getElementById("listaCategorias");
 
 const URL = "http://localhost:8001"
 const endpointCategory = URL + "/category"
+const endpointProduct = URL + "/product"
+
+const form = document.getElementById("formCategory");
+const fieldId = document.getElementById("idCat");
+const fielName = document.getElementById("txtName");
 
 async function loadCategory() {
     try{
@@ -19,8 +24,8 @@ async function loadCategory() {
                         <td>${cat.id}</td>
                         <td>${cat.nome}</td>
                         <td>
-                            <button class="btn btn-info" onclick="preencherForm(${cat.id}, ${cat.nome})">Editar</button>
-                            <button class="btn btn-danger" onclick="excluirCategoria(${cat.id})">Excluir</button>
+                            <button class="btn btn-info" onclick="fillForm('${cat.id}', '${cat.nome}')">Editar</button>
+                            <button class="btn btn-danger" onclick="deleteCategory(${cat.id})">Excluir</button>
                         </td>
                     </tr>
                 `
@@ -33,3 +38,81 @@ async function loadCategory() {
 }
 
 loadCategory();
+
+async function deleteCategory(id){
+    const confirmation = confirm("Confirma exclusão?");
+    
+    if(!confirmation) return;
+
+    try{
+        const response = await fetch(
+            `${endpointCategory}/${id}`,
+            {method: 'DELETE'}
+        )
+
+        if(response.ok){
+            alert("Categoria excluida com sucesso!")
+        }
+    } catch (error) {
+        console.error(error);
+        alert("Erro ao excluir categoria");
+    }
+}
+
+function fillForm(idCat, nameCat){
+    fieldId.value = idCat;
+    fielName.value = nameCat;
+}
+
+async function editCategory(idCat, category){
+    try{
+        const response = await fetch(
+            `${endpointCategory}/${idCat}`,
+            {
+                method: "PUT", 
+                headers: {"Content-Type" : "application/json"},
+                body: JSON.stringify(category)
+            }
+        );
+        if(response.ok){
+            alert("Categoria atualizada com sucesso!");
+            loadCategory();
+        }
+    } catch(error){
+        console.error(error);
+        alert("Erro ao editar categoria");
+    }
+}
+
+async function addCategory(category) {
+    const response = await fetch(
+        endpointCategory,
+        {
+            method: "POST", 
+            headers: {"Content-Type" : "application/json"},
+            body: JSON.stringify(category)
+        }
+    );
+    if(response.ok){
+        alert("Categoria adicionada com sucesso!");
+        loadCategory();
+    }
+    return await response.json();
+}
+
+form.addEventListener("submit", async function(event){
+    event.preventDefault();
+    const idCat = fieldId.value;
+    const category = {nome : fielName.value}
+
+    try{
+        if(idCat){
+            await editCategory(idCat, category);
+        } else{
+            await addCategory(category);
+        }
+    } catch(error){
+        console.error(error);
+        alert("Erro ao adicionar ou editar categoria")
+    }
+});
